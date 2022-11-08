46acc8284df10f64327ab4f62574aec31905312a
limon029 <limon00029@gmail.com>
2022-11-08 10:52:33 +0900

    회원가입 UI 추가

--- Assets/Client/Scripts/UIManager.cs
+++ Assets/Client/Scripts/UIManager.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Text;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -33,7 +33,18 @@ public class UIManager : MonoBehaviour
         public string refreshToken;
     }
 
+    [Serializable]
+    private class SignUpData
+    {
+        public string user_id;
+        public string password;
+        public string email;
+        public string username;
+    }
+
+
     User user = new User();
+    SignUpData signUpData = new SignUpData();
     Data d;
     Token token = new Token();
     public string authorization;
@@ -43,6 +54,8 @@ public class UIManager : MonoBehaviour
     public GameObject startMenu;
     public GameObject multiMenu;
     public GameObject loginMenu;
+    public GameObject signUpMenu;
+    public GameObject signUpPanel;
     public GameObject logintestMenu;
     public GameObject connectingMenu;
     public Button refreshButton;
@@ -53,6 +66,7 @@ public class UIManager : MonoBehaviour
     private Coroutine loginCoroutine = null;
     private Coroutine refreshCoroutine = null;
     private Coroutine logoutCoroutine = null;
+    private Coroutine signUpCoroutine = null;
 
 
 
@@ -198,7 +212,116 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    
+    public void SignUpButtonClicked()
+    {
+        StartCoroutine(IneternetConnectCheck(isConnected =>
+        {
+            if (isConnected)
+            {
+                Debug.Log("Server Available!");
+            }
+            else
+            {
+                Debug.Log("Internet or server Not Available");
+            }
+        }));
+
+        IEnumerator IneternetConnectCheck(Action<bool> action)
+        {
+            using (UnityWebRequest request = new UnityWebRequest("http://localhost:3001/"))
+            {
+                request.downloadHandler = new DownloadHandlerBuffer();
+
+                yield return request.SendWebRequest();
+                if (request.error != null)
+                {
+                    action(false);
+                }
+                else
+                {
+                    action(true);
+                    startMenu.SetActive(false);
+                    signUpMenu.SetActive(true);
+                }
+                request.downloadHandler.Dispose();
+                request.Dispose();
+            }
+        }
+    }
+
+    public void SignUpSubmitButtonClicked()
+    {
+        Dictionary<string, string> datas = new Dictionary<string, string>();
+        int dataCnt = signUpPanel.transform.childCount;
+        for(int i = 0; i < dataCnt; ++i){
+            InputField inputField;
+            signUpPanel.transform.GetChild(i).gameObject.TryGetComponent<InputField>(out inputField);
+            if(inputField != null)
+                datas.Add(inputField.name, inputField.text);
+        }
+
+        Debug.Log(datas.Count);
+
+        if(datas["Id"] == "" || datas["Username"]  == "" || datas["Email"] == "" || datas["Password"] == "")
+            return;
+
+        signUpData.user_id = datas["Id"];
+        signUpData.email = datas["Email"];
+        signUpData.username = datas["Username"];
+        signUpData.password = datas["Password"];
+
+        string json = JsonUtility.ToJson(signUpData);
+
+        if(signUpCoroutine == null) {
+            Debug.Log(json);
+            signUpCoroutine = StartCoroutine(SignUp());
+        } else {
+            Debug.Log("Can not start signInCoroutine. Already logined.");
+        }
+
+        IEnumerator SignUp(){
+            yield return StartCoroutine(SignUpProcess((isProcessDone) =>
+            {
+                signUpMenu.SetActive(false);
+                startMenu.SetActive(true);
+            }));
+            signUpCoroutine = null;
+        }
+
+        IEnumerator SignUpProcess(Action<bool> Result)
+        {
+            using ( UnityWebRequest request = UnityWebRequest.Put("http://localhost:3001/api/user/apply", json))
+            {
+                byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+                request.SetRequestHeader("Content-Type", "application/json");
+
+                request.uploadHandler.Dispose();
+
+                request.uploadHandler = new UploadHandlerRaw(jsonToSend);;
+                request.downloadHandler = new DownloadHandlerBuffer();
+
+                yield return request.SendWebRequest();
+
+                authorization = request.downloadHandler.text;
+
+                string temp = authorization;
+                string json2 = JsonUtility.ToJson(temp);
+
+                if (request.error != null)
+                {
+                    Debug.Log(request.error);
+                }
+                else
+                {
+                    Debug.Log(temp);
+                    Result(true);
+                }
+                request.uploadHandler.Dispose();
+                request.downloadHandler.Dispose();
+                request.Dispose();
+            }
+        }
+    }
         public void RefreshButtonClicked()
     {
         refreshButton.interactable = false;
