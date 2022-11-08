f95920fd1b3973645fdb1927c5903afc9f178e9d
dranger97 <106656036+dranger97@users.noreply.github.com>
2022-11-08 12:57:21 +0900

    Stage 1,2 revised

---
+++ Assets/Client/Scripts/DeathZone.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            collision.transform.GetComponent<PlayerMoveManager>().Hp = 0;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
