4d62895131dc2a2590ba82339538ba2aa2d3c193
Garagekeeper <hj06869@naver.com>
2022-12-01 17:41:10 +0900

    스프라이트 최신화
    스프라이트 최신화

--- Assets/Server/Scripts for editor/MapDataSave.cs
+++ Assets/Server/Scripts for editor/MapDataSave.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.IO;
 using System.Linq;
-
+using System;
 /*
     Made by limmon029 
  */
@@ -14,48 +14,28 @@ public class MapDataSave : MonoBehaviour
     // [SerializeField] GameObject itemGroup;
     // [SerializeField] GameObject playerGroup;
     // [SerializeField] GameObject enemyGroup;
-
-    // TYPE 2: 배열로 받는 경우
-    [SerializeField] GameObject[] items;
-    [SerializeField] GameObject[] players;
-    [SerializeField] GameObject[] enemys;
-
     [SerializeField] private string filePath = "MapData/";
     [SerializeField] string fileName = "MyMap";
 
-    private Dictionary<Vector3, DataClass> infos;
+    public GameObject backGround;
+    private Dictionary<int, DataClass> infos;
     private string fullFilePath;
 
-    private List<string> TileSpriteTypes = System.Enum.GetNames(typeof(TileType)).ToList<string>();
+    //private List<string> TileSpriteTypes = System.Enum.GetNames(typeof(TileType)).ToList<string>();
 
-    private void GetItemInfo()
+
+
+    private int key;
+
+    private void GetMapInfo()
     {
-        // TYPE 1: 그룹으로 받는 경우
-        // if(itemGroup != null) {
-        //     int itemCnt = itemGroup.transform.childCount;
-        //     for (int i = 0; i < itemCnt; ++i) {
-        //         GameObject item = itemGroup.transform.GetChild(i).gameObject;
-        //         Vector3 pos = item.transform.position;
-        //         var itemSprite = item.GetComponent<Sprite>().name;
-        //         if(GoldSpriteTypes.Contains(itemSprite))
-        //             infos.Add(pos, new DataClass((int)InfoTypes.item, pos, (int)ItemTypes.GoldCoin));
-        //         else if(SilverSpriteTypes.Contains(itemSprite))
-        //             infos.Add(pos, new DataClass((int)InfoTypes.item, pos, (int)ItemTypes.SilverCoin));
-        //         else
-        //             infos.Add(pos, new DataClass((int)InfoTypes.item, pos, (int)ItemTypes.BronzeCoin));
-        //     }
-        // }
-
-        // TYPE 2: 배열로 받는 경우
-    }
-
-    private void GetTileInfo()
-    {
+        key = 1;
+        object tileEnum;
         int tileMapCnt = tileMapGrid.transform.childCount;
         for (int i = 0; i < tileMapCnt; ++i)
         {
             Tilemap tileMap = tileMapGrid.transform.GetChild(i).gameObject.GetComponent<Tilemap>();
-                foreach (Vector3Int _pos in tileMap.cellBounds.allPositionsWithin)
+            foreach (Vector3Int _pos in tileMap.cellBounds.allPositionsWithin)
             {
                 Vector3 pos = _pos + new Vector3(0.5f, 0.5f, 0);
 
@@ -64,59 +44,25 @@ public class MapDataSave : MonoBehaviour
 
                 var tile = tileMap.GetTile<TileBase>(_pos);
                 var tileSprite = tileMap.GetSprite(_pos);
-                Debug.Log($" {TileSpriteTypes.IndexOf(tileSprite.name)}, {tileSprite.name}");
-
-                infos.Add(pos, new DataClass((int)TileSpriteTypes.IndexOf(tileSprite.name), pos, TileSpriteTypes.IndexOf(tileSprite.name)));
+                tileEnum = Enum.Parse(typeof(TileType), tileSprite.name);
+                infos.Add(key, new DataClass((int)tileEnum / 100, pos, (int)tileEnum));
+                ++key;
             }
         }
-    }
-
-    private void GetEnemyInfo()
-    {
-        // TYPE 1: 그룹으로 받는 경우
-        // if(enemyGroup != null) {
-        //     int enemyCnt = enemyGroup.transform.childCount;
-        //     for (int i = 0; i < enemyCnt; ++i) {
-        //         GameObject enemy = enemyGroup.transform.GetChild(i).gameObject;
-        //         Vector3 pos = enemy.transform.position;
-        //         var enemySprite = enemy.GetComponent<Sprite>().name;
-        //         if(EnemySpriteTypes.Contains(enemySprite))
-        //             infos.Add(pos, new DataClass((int)InfoTypes.enemy, pos, (int)EnemyTypes.Enemy));
-        //     }
-        // }
-
-        // TYPE 2: 배열로 받는 경우
-    }
-
-    private void GetPlayerInfo()
-    {
-        // TYPE 1: 그룹으로 받는 경우
-        // if(playerGroup != null) {
-        //     int playerCnt = playerGroup.transform.childCount;
-        //     for (int i = 0; i < playerCnt; ++i) {
-        //         GameObject player = playerGroup.transform.GetChild(i).gameObject;
-        //         Vector3 pos = player.transform.position;
-        //         var playerSprite = player.GetComponent<Sprite>().name;
-        //         if(PlayerSpriteTypes.Contains(playerSprite))
-        //             infos.Add(pos, new DataClass((int)InfoTypes.player, pos, (int)PlayerTypes.player));
-        //     }
-        // }
-
-        // TYPE 2: 배열로 받는 경우
+        var backGroundName = backGround.GetComponent<SpriteRenderer>().sprite.name;
+        tileEnum = Enum.Parse(typeof(TileType), backGroundName);
+        infos.Add(key, new DataClass((int)tileEnum / 100, new Vector3(0, 0, 0), (int)tileEnum));
     }
 
     public void Save()
     {
 
-        infos = new Dictionary<Vector3, DataClass>();
+        infos = new Dictionary<int, DataClass>();
         fullFilePath = filePath + fileName;
 
-        GetTileInfo();
-        GetItemInfo();
-        GetPlayerInfo();
-        GetEnemyInfo();
-
-        string toJson = JsonUtility.ToJson(new Serialization<Vector3, DataClass>(infos));
+        GetMapInfo();
+        key = 0;
+        string toJson = JsonUtility.ToJson(new Serialization<int, DataClass>(infos));
         int num = 0;
 
         Debug.Log($"{filePath}");
